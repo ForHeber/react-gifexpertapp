568de49a001f21a9797094c16c5ab7ec3390114a
Heber Flores <heberfloresv@gmail.com>
2021-01-31 12:25:28 -0300

    Primer commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import { getGif } from "../helpers/getGifs";
+
+
+
+export const useFetchGifs = ( category ) => {
+    
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect( () => {
+
+        getGif( category )
+            .then( imgs => {
+                setState({
+                    data: imgs,
+                    loading: false
+                });
+            })
+
+    }, [ category ])
+
+/*     setTimeout(() => {
+        setState({
+            data: [1,2,3,4,5,6],
+            loading: false
+        })
+        
+    }, 3000); */
+
+    return state;
+
+}
